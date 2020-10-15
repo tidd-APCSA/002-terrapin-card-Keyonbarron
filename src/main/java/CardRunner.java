2eb34bdc3fdd42ca29d04858164e0d35393a64cb
Keyonbarron <supbrysi@gmail.com>
2020-10-15 16:22:09 +0000

    1st check

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,7 +1,28 @@
 public class CardRunner {
     public static void main(String args[]){
         // ☕
-        System.out.println("hey");
-
+        
+        TerrapinCard niceBilly = new TerrapinCard(20);
+        TerrapinCard niceBrian = new TerrapinCard(30);
+        
+        niceBilly.payGourmet();
+        niceBrian.payEconomical();
+        
+        System.out.println("Billy: "+niceBilly);
+        System.out.println("Brian: "+niceBrian);
+        
+        niceBilly.loadMoney(20);
+        niceBrian.payGourmet();
+        
+        System.out.println("Billy: "+niceBilly);
+        System.out.println("Brian: "+niceBrian);
+        
+        niceBilly.payEconomical();
+        niceBilly.payEconomical();
+        niceBrian.loadMoney(50);
+        
+        System.out.println("Billy: "+niceBilly);
+        System.out.println("Brian: "+niceBrian);
+        
     }
 }
